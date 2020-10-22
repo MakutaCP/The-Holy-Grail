723a1a089344cb8d171ea5eb357b07baee166657
MakutaCP <makutacp@gmail.com>
2020-10-22 17:40:54 -0400

    basic experiments

---
+++ holygrail.py
@@ -0,0 +1,9 @@
+grail = "The Holy Grail"
+
+grail = False
+if grail is True:
+    print("Well Done! You have beaten the game!")
+else:
+    print("Welcome to the adenture!")
+
+
---
+++ player.py
@@ -0,0 +1,10 @@
+class Player:
+    grail = False
+    def __init__(self, name):
+        self.name = name
+    
+        
+
+p1 = Player("Carl")
+
+print(p1.name)
