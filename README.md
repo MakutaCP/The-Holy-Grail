5cb8cf9f8454f3e6f4a8a5bf1cc7af9225bc54a5
MakutaCP <makutacp@gmail.com>
2020-11-19 22:40:35 -0500

    Final edits

--- README.md
+++ README.md
@@ -2,4 +2,16 @@
 
 Python project for code louisville
 
-Type begin to start. if the you reach an ending you can type begin to restart the adventure.
+This project should be able to be run easily through the python shell in the standered way. No special instructions for operation.
+
+Type "begin" to start. If the you reach an ending you can type "begin" to restart the adventure.
+
+For this project I have:
+
+1. A master loop that alows you to play again.
+
+2. At least three function with one of them returning something used on the program.
+
+3. Several lists populated with values and retrieved to be used in the program.
+
+4. Possibly a text analysis to display information.
--- holygrail.py
+++ holygrail.py
@@ -15,8 +15,9 @@ def welcome():
     What()
 
 def What():
-
+    #Just some for fun questions here and the first sudden ending
     yes_or_no_list = []
+    yes_or_no_list.clear
 
     yes_or_no_list.append(input("You would like to earn the grail yes?  ").lower())
 
@@ -29,21 +30,20 @@ def What():
         questions()
     elif "no" in yes_or_no_list:
         print("   ")
-        print("Oh. You must be looking for the batrooms then. Those are three doors down on the left.")
+        print("Oh. You must be looking for the bathrooms then. Those are three doors down on the left.")
         print("   ")
         print("Off you go then.")
         print("   ")
         print("--THE END?--")
+        print("--TYPE BEGIN TO START AGAIN OR TYPE QUIT TO EXIT-- ")
     else:
         print("   ")
         print("What was that? It is so hard to hear in this helmet.")
         print("   ")
         What()
 
-def questions():
-    questions_list = []
-    answer_list = []
-
+def ask_questions(questions_list):
+    #This function is called below and asks the questions before returning the values
     print("   ")
     questions_list.append( input("What is your name?  "))
 
@@ -56,6 +56,15 @@ def questions():
     print("Really? Well then the final question!")
     print("   ")
     questions_list.append( input("What is your favorite color?  "))
+    return questions_list
+
+def questions():
+    #This function takes the length of the answers, adds them together,
+    #and returns the number which is used to give one of three riddles.
+    empty_list = []
+    answer_list = []
+
+    questions_list = ask_questions(empty_list)
 
     answer_list.append( len(questions_list[0]))
     answer_list.append( len(questions_list[1]))
@@ -63,7 +72,7 @@ def questions():
 
     truth = sum(answer_list)
     print("   ")
-    print("Your answers gives me the number...")
+    print("Your answers gave me the number...")
     print(truth)
 
     print("   ")
@@ -75,68 +84,101 @@ def questions():
         Rid1()
     elif truth >= 30 and truth <= 60:
         print("   ")
-        print("A average adventuring type. Above average in general. You shall have this riddle!")
+        print("An average adventuring type. Above average in general. You shall have this riddle!")
         print("   ")
+        Rid2()
     elif truth > 60:
         print("   ")
         print("A very wordy person. Maybe a wizard? Well a fitting riddle I will give you!")
         print("   ")
+        Rid3()
     else:
         print("   ")
-        print("Wait a second some thing is fishy here. And it is not my fish Eric.")
+        print("Wait a second something is fishy here. And it is not my fish Eric.")
         print("   ")
         print("You get out you cheater. You are not worthy!")
         print("   ")
         print("--THE END.--")
+        print("--TYPE BEGIN TO START AGAIN OR TYPE QUIT TO EXIT-- ")
 
+
+def congrats():
+    print("   ")
+    print("--CONGRATULATIONS! YOU HAVE FOUND THE HOLY GRAIL--")
+    print("   ")
+    print("--TYPE BEGIN TO START AGAIN OR TYPE QUIT TO EXIT--")
+
+def fail():
+    print("   ")
+    print("--YOU HAVE FAILED. PLEASE LEAVE AND NEVER RETURN!--")
+    print("   ")
+    print("--TYPE BEGIN TO START AGAIN OR TYPE QUIT TO EXIT-- ")
 
 def Rid1():
-    pass
+    Rid1_ans = []
+    print("   ")
+    print("There are 3 cups on the table in front of you.")
+    print("   ")
+    print("The 1st is red, the 2nd is blue, and the 3rd is yellow.")
+    print("   ")
+    Rid1_ans.append(input("Give me the number of the cup that does not make the color green. That is the Holy Grail.  "))
+
+    if Rid1_ans[0] == "1":
+        congrats()
+    else:
+        fail()
+    
 
 def Rid2():
     Rid2_ans = []
     print("   ")
-    print("There are five cups on the table infront of you.")
+    print("There are 5 cups on the table infront of you.")
     print("   ")
-    print("The first three are full of water while the fourth and fifth is empty.")
+    print("From left to right the first 3 are full of water while the 4th and 5th are empty.")
     print("   ")
-    print("Only one cup needs to be moved to make make the first, third, and fifth cups full of water.")
+    print("Only 1 cup needs to be moved to make the 1st, 3rd, and 5th cups full of water.")
     print("   ")
-    Rid2_ans.append( input("Give me the number of which cup needs to move.  "))
+    Rid2_ans.append( input("Give me the number of which cup needs to move. That is the Holy Grail.  "))
 
-    if Rid2_ans == "2":
+    if Rid2_ans[0] == "2":
         congrats()
     else:
         fail()
 
 
 def Rid3():
-    pass
+    Rid3_ans = []
+    print("   ")
+    print("Five cups on the table, three saints up above,")
+    print("Two full of hate, three full of love,")
+    print("Two hates not together, for fear of a crime,")
+    print("Two loves together, forever in time,")
+    print("The one love alone is the Holy Grail you seek,")
+    print("Not next to the loves, as a gift to the meek,")
+    print("The 1st on the left is not the Grail, not the boon")
+    print("But the hate on the other end shall be your doom.")
+    print("   ")
+    print("Which cup is the Holy Grail?")
+    print("   ")
+    Rid3_ans.append( input("Give me the number of the Grail. The answer wll be 1,2,3,4, or 5.  "))
+
+
+
+    if Rid3_ans[0] == "4":
+        congrats()
+    else:
+        fail()
+
 
     
 
 start_up()
 while True:
+    #Allows for the repeat of the game as well as quiting. 
     new_item = input("> ").lower()
     if new_item == "begin":
         welcome()
-    elif new_item == "result":
-        secret()
     elif new_item == "quit":
         exit()
     else:
         print("-Please enter a valid command-")
-
-def congrats():
-    print("   ")
-    print("--CONGRADULATIONS! YOU HAVE FOUND THE HOLY GRAIL--")
-    print("   ")
-    print("--GIVE THE COMMAND RESULT AT THE BEGINING TO FIND YOUR SECRET--")
-
-def fail():
-    print("   ")
-    print("--YOU HAVE FAILED. PLEASE LEAVE AND NEVER RETURN!--")
-
-def secret():
-    print("HOW DID YOU FIND THIS PLACE?!?")
-    print("YOU MUST HAVE THE GRAIL.")
